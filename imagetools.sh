62edb95d2c76a6cb4533ec59fc268b6b60ad4d24
bluicezhen <bluicezhen@tencent.com>
2020-09-29 18:20:40 +0800

    Fix startup script: can't get working directory

--- imagetools.sh
+++ imagetools.sh
@@ -4,7 +4,5 @@ export PIPENV_IGNORE_VIRTUALENVS=1
 IMAGETOOLS_PATH=$(greadlink -f "$0")
 IMAGETOOLS_DIR=$(dirname "$IMAGETOOLS_PATH")
 
-cd "$IMAGETOOLS_DIR" || exit 1
-
-PYTHON_INTERPRETER=$(pipenv --py)
-"$PYTHON_INTERPRETER" imagetools.py "$@"
+PYTHON_INTERPRETER=$(cd "$IMAGETOOLS_DIR" && pipenv --py)
+"$PYTHON_INTERPRETER" "$IMAGETOOLS_DIR/imagetools.py" "$@"
